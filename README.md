2658af6f810b0819a7c7860994c5b31a8c40815d
Pablo Schaffner (office) <pablo@creador.cl>
2017-01-30 22:12:45 -0300

    Added ability to extract and decrypt classes.dex file

--- README.md
+++ README.md
@@ -3,7 +3,13 @@ APK Unpack
 ## INTRO
 
 This package and command-line helps you unpack any APK (get its smali sources) and decrypt its manifest.  
-It contains 2 main methods:
+It contains 3 main methods:
+
+### init (config).  
+Initializes the component. Can have the keys:<br/>
+apk (apkfile to open),<br/>
+dir (outputdir),<br/>
+java (true if you want the decrypted classes.jar)<br/><br/>
 
 ### extract (apkfile, outputdir, onReadyCallback).  
 This does the extraction of assets and sources into the given outputdir.  
@@ -17,6 +23,9 @@ It comes with a commandline that you can use as follows:
 **apk_unpack** `apkfile.apk` `outputdir`
 
 ## UPDATES
+
+version 1.1.2:
+- added ability to extract classes.dex and decode its contents using dex2jar
 
 version 1.1.1:
 - Fixed java class location for using command-line as bin
--- apk_unpack.js
+++ apk_unpack.js
@@ -5,37 +5,63 @@ var	fs 			=	require('fs'),
 	java 		= 	require('java'),
 	path 		= 	require('path'),
 	_last		= 	{ dir:'' },
-	cwd 		= 	process.cwd();
+	cwd 		= 	process.cwd(),
+	_config 	= 	{
+		apk 		: 	'',
+		java 		: 	true,
+		dir 		: 	''
+	};
 	
 java.classpath.push(__dirname+path.sep+'java/apktool_2.2.2.jar');
+// add classes for DEX 2 JAR
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'antlr-runtime-3.5.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'asm-debug-all-4.1.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'d2j-base-cmd-2.0.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'d2j-jasmin-2.0.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'d2j-smali-2.0.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'dex-tools-2.0.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'dex-ir-2.0.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'dex-reader-api-2.0.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'dex-writer-2.0.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'dex-translator-2.0.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'dex-reader-2.0.jar');
+java.classpath.push(__dirname+path.sep+'java/dex2jar/'+'dx-1.7.jar');
+
+//
 var	classes 	= 	{
 	ApkDecoder 		: 	java.import('brut.androlib.ApkDecoder'),
 	libResources 	: 	java.import('brut.androlib.res.AndrolibResources'),
 	ExtFile 		: 	java.import('brut.directory.ExtFile'),
-	File 			:	java.import('java.io.File')
+	File 			:	java.import('java.io.File'),
+	//dex2jar
+	_dex2jar 	 	: 	java.import('com.googlecode.d2j.dex.Dex2jar'),
+	_dexFileReader	: 	java.import('com.googlecode.d2j.reader.DexFileReader'),
+	_dexZipUtil		: 	java.import('com.googlecode.d2j.reader.zip.ZipUtil')
 };
 
-var extractAPK = function(apkfile, outputdir, cb) {
-	var _apk = apkfile,
-		_outd = outputdir;
-	if (_apk.charAt(0)!=path.sep && _apk.charAt(0)!='~') {
-		_apk = path.join(cwd,_apk);
+var init = function(config) {
+	for (var i in config) {
+		_config[i] = config[i];
 	}
-	if (_outd.charAt(_outd.length-1)!=path.sep) {
+	// prepare apk and outputdir location
+	var _apk = _config.apk, _outd = _config.dir;
+	if (_config.apk.charAt(0)!=path.sep && _config.apk.charAt(0)!='~') {
+		_config.full_apk = path.join(cwd,_config.apk);
+	}
+	if (_config.dir.charAt(_config.dir.length-1)!=path.sep) {
 		// if outputdir doesn't end in / or path separator, add it.
-		_outd = _outd+''+path.sep;
+		_config.full_dir = _config.dir+''+path.sep;
 	}
-	//console.log('apkfile:'+_apk,outputdir);
-	/*if (!dirExists(outputdir)) {
-		fs.mkdirSync(outputdir);
-	}*/
-	if (fileExists(apkfile)) {
-		_last.dir = _outd;
+};
+
+var extractAPK = function(cb) {
+	if (fileExists(_config.apk)) {
+		_last.dir = _config.full_dir;
 		var decoder = new classes.ApkDecoder();
-		var _apkfile = new classes.File(_apk);
+		var _apkfile = new classes.File(_config.full_apk);
 		decoder.setForceDeleteSync(true);
 		decoder.setDecodeResourcesSync(java.newShort(256));	// exclude resources
-		decoder.setOutDirSync(new classes.File(_outd));
+		decoder.setOutDirSync(new classes.File(_config.full_dir));
 		decoder.setApkFileSync(_apkfile);
 		decoder.decodeSync();
 		// try to decrypt manifest
@@ -43,12 +69,59 @@ var extractAPK = function(apkfile, outputdir, cb) {
 		var _res = decoder.getResTableSync();
 		var _ext = new classes.ExtFile(_apkfile);
 		var _lib = new classes.libResources();
-		_lib.decodeManifest(_res, _ext, new classes.File(_outd), function() {
+		_lib.decodeManifest(_res, _ext, new classes.File(_config.full_dir), function() {
 			cb(true);
 		});
 	} else {
 		cb(false);
 	}
+};
+
+var decompile = function(onReady) {
+	// extract classes.dex into output
+	extract_dex(function() {
+		// decrypt dex into jar, using dex2jar
+		dexToJar(function() {
+			// get java code using jd-cli
+			onReady();
+		});
+	});
+};
+
+var dexToJar = function(onReady) {
+	// decrypt dex into jar, using dex2jar
+	_last.dex  		=  	_last.dir + 'classes.dex';
+	_last.dexjar 	=  	_last.dir + 'classes.jar';
+	var _dex 		= 	new classes.File(_last.dex);
+	var _dexjar 	= 	new classes.File(_last.dexjar);
+	// delay for releaasing classes.dex 
+	setTimeout(function(){
+		var _reader 	= 	new classes._dexFileReader(_dex);
+		classes._dex2jar.fromSync(_reader).toSync(_dexjar.toPathSync());
+		onReady();
+	}, 300);
+};
+
+var extract_dex = function(onReady) {
+	// extract classes.dex from apk into outputdir.
+	var yauzl = require('yauzl');
+	yauzl.open(_config.apk, { lazyEntries: false }, function(err,zipfile) {
+		if (err) throw err;
+		//  zipfile.readEntry();
+		zipfile.on('entry', function(entry) {
+			if (!/\/$/.test(entry.fileName)) {
+				// file
+				zipfile.openReadStream(entry, function(err2, readStream) {
+					//if (err2) throw err2;
+					if (entry.fileName=='classes.dex') {
+						readStream.pipe(fs.createWriteStream(_last.dir + entry.fileName));
+						zipfile.close();
+						onReady();
+					}
+				});
+			}
+		});
+	});
 };
 
 var info = function(callback) {
@@ -97,5 +170,7 @@ var dirExists = function(filePath)
     }
 }
 
+exports.init = init;
 exports.extract = extractAPK;
+exports.decompile = decompile;
 exports.info = info;
